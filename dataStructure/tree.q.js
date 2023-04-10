c32effd5e10462dd53b799319b10350795e790fc
DESKTOP-NSNJVU7\runcj <run1529@gmail.com>
2023-04-10 17:37:05 +0900

    자료구조 tree delete 구현

--- dataStructure/tree.q.js
+++ dataStructure/tree.q.js
@@ -34,27 +34,79 @@ const makeTree = (initVal) => {
                 if(value > node.value) {
                     if(!node.right) {
                         if(node.value !== value) {
-                            console.log("false");
+                            return false
                         }
                         break;
                     }
                     node = node.right;
-                    if(node.value === value) {
-                        console.log("true");
-                    }
+                } else if(node.value === value) {
+                    return true
                 } else {
                     if(!node.left) {
                         if(node.value !== value) {
-                            console.log("false");
+                            return false
                         }
                         break;
                     }
                     node = node.left;
-                    if(node.value === value) {
-                        console.log("true");
+                }
+            }
+        },
+        delete:function (value) {
+            let node = this.head;
+            let parent = null
+            let isHeadTarget = false
+
+            while(true) {
+                if(value > node.value) {
+                    if(value === node.right.value) {
+                        parent = node
+                    }
+                    node = node.right
+                } else if(value < node.value) {
+                    if(value === node.left.value) {
+                        parent = node
+                    }
+                    node = node.left
+                }
+
+                if(parent) {
+                    if(!node.right) {
+                        if(parent.value < node.value) {
+                            parent.right = node.left
+                            return
+                        } else {
+                            parent.left = node.left
+                            return
+                        }
+                    }
+
+                    const targetNode = node // 지워야 할 노드
+                    node = node.right
+
+                    while(true) {
+                        if(node.left.left) {
+                            node = node.left
+                        } else {
+                            const tempNode = node.left
+                            // right 가 있는 경우
+                            if(node.left.right) {
+                                node.left = node.left.right
+                            }
+                            tempNode.left = targetNode.left
+                            tempNode.right = targetNode.right
+                            if(parent.value > value) {
+                                parent.left = tempNode
+                                return
+                            } else {
+                                parent.right = tempNode
+                                return
+                            }
+                        }
                     }
                 }
             }
+
         }
     }
 }
@@ -62,6 +114,8 @@ const tree = makeTree(10);
 tree.insert(15);
 tree.insert(1);
 tree.insert(4);
+tree.insert(7);
 
-//tree
-tree.search(4);
+tree.delete(15)
+const t = tree.search(15)
+t
