a7f1aeea27c33088ec05dcd5612e58d2d80388e2
choe9 <run1529@gmail.com>
2023-04-19 01:00:14 +0900

    day18 백준문제풀기

--- baekjoon/2023/04/day07_7/2439/2439.q.js
+++ baekjoon/2023/04/day07_7/2439/2439.q.js
@@ -1,6 +1,9 @@
 // const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const input = fs.readFileSync(__dirname + "/test.txt").toString().trim();
+const input = fs
+  .readFileSync(__dirname + "/test.txt")
+  .toString()
+  .trim();
 
 // for(let i = 0; i < Number(input[0]); i++) {
 //     star += "\n";
@@ -14,10 +17,8 @@ const input = fs.readFileSync(__dirname + "/test.txt").toString().trim();
 //star = star.substring(1);
 
 let count = Number(input);
-for(let i = 0; i < count; i++) {
-    const emptyCount = count-i-1
+for (let i = 0; i < count; i++) {
+  const emptyCount = count - i - 1;
 
-        console.log(' '.repeat(emptyCount)+'*'.repeat(i+1))
-
+  console.log(" ".repeat(emptyCount) + "*".repeat(i + 1));
 }
-
