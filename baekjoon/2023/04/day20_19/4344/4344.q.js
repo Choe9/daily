244da593f4bdeec1a50f137374451d594190ddb6
choe9 <run1529@gmail.com>
2023-04-21 14:14:59 +0900

    백준문제풀기 평균은 넘곘지 문제 제출

--- baekjoon/2023/04/day20_19/4344/4344.q.js
+++ baekjoon/2023/04/day20_19/4344/4344.q.js
@@ -6,31 +6,25 @@ const [n, ...input] = require("fs")
   .split("\n");
 
 let arr = [];
-let arr2 = [];
-let sum = 0;
 let avg = [];
+let classAvg = [];
 for (let i = 0; i < input.length; i++) {
   arr.push(input[i].split(" ").map(Number));
 }
 for (let j = 0; j < arr.length; j++) {
+  let sum = 0;
   for (let k = 0; k < arr[j][0]; k++) {
     sum += arr[j][k + 1];
-    console.log(sum);
   }
-  console.log(avg);
   avg.push(sum / arr[j][0]);
 }
-
-// arr.forEach((element, index) => {
-//     obj[index + `${arr[4][0]}`] = element;
-//   });
-
-// input.forEach((t) => {
-//   const [start] = t.split(" ").map(Number);
-//   const first = t.slice(1, start);
-//   console.log(first);
-//   console.log(t.split(" ").map(Number));
-//   arr = [...t];
-// });
-console.log(arr);
-console.log(avg);
+for (let a = 0; a < avg.length; a++) {
+  let count = 0;
+  for (let b = 0; b < arr[a][0]; b++) {
+    if (arr[a][b + 1] > avg[a]) {
+      count++;
+    }
+  }
+  classAvg.push(((count / arr[a][0]) * 100).toFixed(3));
+}
+console.log(classAvg.join("%\n") + "%");
