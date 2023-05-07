11c14c30b38f1074a6db4b8c4e811129b38bec9b
choe9 <run1529@gmail.com>
2023-05-07 23:36:16 +0900

    day07 백준문제풀기

---
+++ baekjoon/2023/05/day07_4/2869/2869.q.js
@@ -0,0 +1,13 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(__dirname + "/test.txt")
+  .toString()
+  .trim()
+  .split(" ");
+
+const A = input[0]; // 아침에 이동하는 수
+const B = input[1]; // 밤에 미끄러지는 수
+const V = input[2]; // 높이
+
+// (높이 - 밤에 미끄러지는 수) / (아침에 이동하는 수 - 밤에 미끄러지는 수)
+console.log(Math.ceil((V - B) / (A - B)));
