0ba6a5be7876fbffb6fc29bc056cd83dc330ba81
choe9 <run1529@gmail.com>
2023-05-25 13:23:50 +0900

    day25 백준문제풀기

---
+++ baekjoon/2023/05/day25_15/24262/24262.q.js
@@ -0,0 +1,8 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(__dirname + "/test.txt")
+  .toString()
+  .trim();
+
+console.log(1);
+console.log(0);
